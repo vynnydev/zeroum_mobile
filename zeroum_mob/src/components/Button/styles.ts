185981ce9a32cd52eda9893f13fbdc96bfd0d958
vynnydev <vynnyprudencio@gmail.com>
2020-08-14 18:19:20 -0300

    feat(fullstack): developed mobile structured project

---
+++ zeroum_mob/src/components/Button/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
+
+export const Container = styled(RectButton)`
+  width: 100%;
+  height: 60px;
+  background: #00813c;
+  border-radius: 10px;
+
+  justify-content: center;
+  align-items: center;
+  margin-top: 8px;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: 'Arial';
+  color: ${(props) => props.theme.colors.text};
+  font-size: 18px;
+`;
